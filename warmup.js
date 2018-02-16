b7a56cf755795254f67663379731b754c7a6472d
Kin Siu <kina.siu@gmail.com>
2018-02-16 09:58:02 -0500

    It is now a js file

---
+++ warmup.js
@@ -0,0 +1,11 @@
+let arrayMe = ["total recall", 42, "istanbul", true, 1911, null, 11, 77777];
+console.log(arrayMe);
+
+arrayMe.push("bamboo", "johnson");
+console.log(arrayMe);
+
+arrayMe.splice(1, 1, 1)
+console.log(arrayMe);
+
+arrayMe.unshift("me first");
+console.log(arrayMe);
